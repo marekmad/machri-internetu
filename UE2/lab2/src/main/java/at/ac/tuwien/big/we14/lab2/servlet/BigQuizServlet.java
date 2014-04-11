b45de891ba19d278001012dffeea19f49c961d1f
Filip Rydzi <rydzi.filip@gmail.com>
2014-04-11 13:22:29 +0200

    ue2

--- UE2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -5,10 +5,14 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import at.ac.tuwien.big.we14.lab2.api.impl.SimpleQuestion;
 
 
 public class BigQuizServlet extends HttpServlet {
@@ -17,6 +21,8 @@ public class BigQuizServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	
+	private static int counter = 0;
 	
     
     @Override
@@ -31,19 +37,66 @@ public class BigQuizServlet extends HttpServlet {
     	String [] selected = request.getParameterValues("opts");
     	
     	
-    	if(selected != null){
-    		response.setContentType("text/html;charset=UTF-8");
-            PrintWriter out = response.getWriter();
-            out.print("<html><body>");
-            out.print("<h1> Your selections");
-            out.print("<ul>");
-            for(String s : selected){
-            	out.print("<li>"+s+"</li>");
-            }
-            out.print("</ul>");
-            out.print("</body></html>");
+//    	if(selected != null) {
+//    		response.setContentType("text/html;charset=UTF-8");
+//            PrintWriter out = response.getWriter();
+//            out.print("<html><body>");
+//            out.print("<h1> Your selections");
+//            out.print("<ul>");
+//            for(String s : selected){
+//            	out.print("<li>"+s+"</li>");
+//            }
+//            out.print("</ul>");
+//            out.print("</body></html>");
+//    	}
+    	
+    	SimpleQuestion question = new SimpleQuestion();
+    	question.setText("Jak sa volas ? "+(++counter));
+    	
+    	HttpSession session = request.getSession(true);
+    	
+    	session.setAttribute("question", question);
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/question.jsp");  
+    	if (dispatcher != null){  
+    		dispatcher.forward(request, response);  
     	}
-    
+    	
+    	
     }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    	
+    	String [] selected = request.getParameterValues("opts");
+    	
+    	
+//    	if(selected != null) {
+//    		response.setContentType("text/html;charset=UTF-8");
+//            PrintWriter out = response.getWriter();
+//            out.print("<html><body>");
+//            out.print("<h1> Your selections");
+//            out.print("<ul>");
+//            for(String s : selected){
+//            	out.print("<li>"+s+"</li>");
+//            }
+//            out.print("</ul>");
+//            out.print("</body></html>");
+//    	}
+    	
+    	SimpleQuestion question = new SimpleQuestion();
+    	question.setText("cigan ?");
+    	
+    	HttpSession session = request.getSession(true);
+    	
+    	session.setAttribute("question", question);
+    	RequestDispatcher dispatcher = request.getRequestDispatcher("/question.jsp");  
+    	if (dispatcher != null){  
+    		dispatcher.forward(request, response);  
+    	}
+    	
+    	
+    }
+    
 
 }
