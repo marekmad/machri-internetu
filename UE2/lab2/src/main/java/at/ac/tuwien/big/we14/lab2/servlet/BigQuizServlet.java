d128c1d050e0525fe72846798a3cf83933f5d2f4
Filip Rydzi <rydzi.filip@gmail.com>
2014-04-23 01:51:48 +0200

    BigQuizServlet, more sessions can run independent

--- UE2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
+++ UE2/lab2/src/main/java/at/ac/tuwien/big/we14/lab2/servlet/BigQuizServlet.java
@@ -37,7 +37,7 @@ public class BigQuizServlet extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	private Game game = new Game();
+	
 
 	@Override
 	public void init() throws ServletException {
@@ -49,55 +49,66 @@ public class BigQuizServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
-		String[] selected = request.getParameterValues("opts");
+		String action = request.getParameter("action");
 		
-		game.validateAnswer(selected);
-
-		if (game.getQuestionNumber() == 3) {
-
-			System.out.println("incrementing score after round "+game.getRoundNumber());
-			game.incrementScoreAfterRound();
-			
-			if (game.getRoundNumber() == 5) {
+		
+		if(action != null){
+			if(action.equals("submitAnswer")){
+				String[] selected = request.getParameterValues("opts");
 				
+				Game game = (Game)request.getSession().getAttribute("game");
 				
-				System.out.println("end of game: ");
-				System.out.println("p1 score: " + game.getPlayer1().getNumberRoundWon());
-				System.out.println("p2 score: " + game.getPlayer2().getNumberRoundWon());
-				
-				
-				RequestDispatcher dispatcher = request
-						.getRequestDispatcher("/finish.jsp");
+				game.validateAnswer(selected);
 
-				if (dispatcher != null) {
-					dispatcher.forward(request, response);
+				if (game.getQuestionNumber() == 3) {
 
+					System.out.println("incrementing score after round "+game.getRoundNumber());
+					game.incrementScoreAfterRound();
+					
+					if (game.getRoundNumber() == 5) {
+						
+						
+						System.out.println("end of game: ");
+						System.out.println("p1 score: " + game.getPlayer1().getNumberRoundWon());
+						System.out.println("p2 score: " + game.getPlayer2().getNumberRoundWon());
+						
+						
+						RequestDispatcher dispatcher = request
+								.getRequestDispatcher("/finish.jsp");
+
+						if (dispatcher != null) {
+							dispatcher.forward(request, response);
+
+						}
+					} else {
+						
+						RequestDispatcher dispatcher = request
+								.getRequestDispatcher("/roundcomplete.jsp");
+
+						if (dispatcher != null) {
+							dispatcher.forward(request, response);
+						}
+					}
+
+				} else {
+
+					HttpSession session = request.getSession(true);
+
+					game.nextQuestion();
+					
+					session.setAttribute("game", game);
+					game.getAcctuallQuestion().setAskedTime(new Date());
+					RequestDispatcher dispatcher = request
+							.getRequestDispatcher("/question.jsp");
+
+					if (dispatcher != null) {
+						dispatcher.forward(request, response);
+					}
 				}
-			} else {
-				
-				RequestDispatcher dispatcher = request
-						.getRequestDispatcher("/roundcomplete.jsp");
-
-				if (dispatcher != null) {
-					dispatcher.forward(request, response);
-				}
-			}
-
-		} else {
-
-			HttpSession session = request.getSession(true);
-
-			game.nextQuestion();
-			
-			session.setAttribute("game", game);
-			game.getAcctuallQuestion().setAskedTime(new Date());
-			RequestDispatcher dispatcher = request
-					.getRequestDispatcher("/question.jsp");
-
-			if (dispatcher != null) {
-				dispatcher.forward(request, response);
 			}
 		}
+		
+		
 
 	}
 
@@ -109,31 +120,47 @@ public class BigQuizServlet extends HttpServlet {
 
 		System.out.println("Action: " + action);
 
-		if (action.equals("newGame")) {
-			startGame();
+		if(action != null){
+			
+			if (action.equals("newGame")) {
+				
+				Game game = new Game();
+				startGame(game);
+				game.nextQuestion();
+				game.getAcctuallQuestion().setAskedTime(new Date());
+				
+				HttpSession session = request.getSession(true);
+				
+				session.setAttribute("game", game);
+				
+			}
+
+			if (action.equals("newRound")) {
+				
+				Game game = (Game)request.getSession().getAttribute("game");
+				game.startNewRound();
+				game.nextQuestion();
+				game.getAcctuallQuestion().setAskedTime(new Date());
+				
+				HttpSession session = request.getSession(true);
+				session.setAttribute("game", game);
+				
+				
+				System.out.println("starting new round " + game.getRoundNumber());
+			}
+			
+			RequestDispatcher dispatcher = request
+					.getRequestDispatcher("question.jsp");
+			if (dispatcher != null) {
+				dispatcher.forward(request, response);
+			}
+			
 		}
-
-		if (action.equals("newRound")) {
-			game.startNewRound();
-			System.out.println("starting new round " + game.getRoundNumber());
-		}
-
-		game.nextQuestion();
-
-		HttpSession session = request.getSession(true);
-
-		session.setAttribute("game", game);
-		game.getAcctuallQuestion().setAskedTime(new Date());
 		
-		RequestDispatcher dispatcher = request
-				.getRequestDispatcher("question.jsp");
-		if (dispatcher != null) {
-			dispatcher.forward(request, response);
-		}
 
 	}
 	
-	public void startGame(){
+	public void startGame(Game game){
 		ServletContext servletContext = getServletContext();
 		QuizFactory factory = ServletQuizFactory.init(servletContext);
 		QuestionDataProvider provider = factory.createQuestionDataProvider();
