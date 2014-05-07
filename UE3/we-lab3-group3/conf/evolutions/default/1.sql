35bc6780320cba666d54d88bcc74076e9c16e870
Filip Rydzi <rydzi.filip@gmail.com>
2014-05-07 17:21:59 +0200

    added DB taktik

---
+++ UE3/we-lab3-group3/conf/evolutions/default/1.sql
@@ -0,0 +1,28 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user (
+  id                        bigint auto_increment not null,
+  vorname                   varchar(255),
+  nachname                  varchar(255),
+  geburts_datum             timestamp,
+  geschlecht                integer,
+  user_name                 varchar(255),
+  passwort                  varchar(255),
+  constraint ck_user_geschlecht check (geschlecht in (0,1)),
+  constraint pk_user primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists user;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
