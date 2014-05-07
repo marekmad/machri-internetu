9436fcad2dc88319c4182cac8932453eec1eb732
Filip Rydzi <rydzi.filip@gmail.com>
2014-05-07 14:29:42 +0200

    registration taktik

---
+++ UE3/we-lab3-group3/app/controllers/Registration.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import static play.data.Form.form;
+import models.User;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.*;
+import views.html.registration;
+
+public class Registration extends Controller {
+	
+	final static Form<User> registrationForm = form(User.class);
+	/**
+     * Display a blank form.
+     */ 
+    public static Result blank() {
+        return ok(registration.render(registrationForm));
+    }
+    public static Result submit() {
+        return ok(registration.render(registrationForm));
+    }
+  
+}
---
+++ UE3/we-lab3-group3/app/models/User.java
@@ -0,0 +1,103 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import play.data.validation.Constraints;
+
+
+@Entity
+public class User {
+	
+	public enum Geschlecht {
+		maenlich, weiblich;
+		
+		public static List<String> toList(){
+			List<String> geschlecht = new ArrayList<String>();
+			geschlecht.add("Manlich");
+			geschlecht.add("Weiblich");
+			return geschlecht;
+		}
+	}
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	String vorname;
+	String nachname;
+	Date geburtsDatum;
+	Geschlecht geschlecht;
+	
+	@Constraints.Required
+	@Constraints.MinLength(4)
+	@Constraints.MaxLength(8)
+	String userName;
+	
+	@Constraints.Required
+	@Constraints.MinLength(4)
+	@Constraints.MaxLength(8)
+	String passwort;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getVorname() {
+		return vorname;
+	}
+
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+
+	public String getNachname() {
+		return nachname;
+	}
+
+	public void setNachname(String nachname) {
+		this.nachname = nachname;
+	}
+
+	public Date getGeburtsDatum() {
+		return geburtsDatum;
+	}
+
+	public void setGeburtsDatum(Date geburtsDatum) {
+		this.geburtsDatum = geburtsDatum;
+	}
+
+	public Geschlecht getGeschlecht() {
+		return geschlecht;
+	}
+
+	public void setGeschlecht(Geschlecht geschlecht) {
+		this.geschlecht = geschlecht;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPasswort() {
+		return passwort;
+	}
+
+	public void setPasswort(String passwort) {
+		this.passwort = passwort;
+	}
+	
+	
+	
+}
